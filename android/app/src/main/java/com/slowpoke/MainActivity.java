a58f3a86fbcf5b82e9c74a82f0bb6f5f03a6bc0f
Nate <nate.hunzaker@gmail.com>
2019-03-19 13:34:09 -0700

    First commit

---
+++ android/app/src/main/java/com/slowpoke/MainActivity.java
@@ -0,0 +1,8 @@
+package com.slowpoke;
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+    // Updated according to:
+    // https://wix.github.io/react-native-navigation/#/docs/Installing
+}
